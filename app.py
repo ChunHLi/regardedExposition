6db7a6967709ccf819bd4992e6bcbcaaadc35df8
Albert Mokrejs <albertmokrejs@gmail.com>
2015-11-22 17:51:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def whenSearch(query):
     
     possibleDates = sorted(partDict.iteritems(), key=itemgetter(1), reverse=True)
     
-    return possibleDates[0]
+    return possibleDates[0][0]
 
 if __name__ == "__main__":
    app.debug = True

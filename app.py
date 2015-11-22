36ce2a1c5da0e4d832b8db5c94abd210d27b0757
Albert Mokrejs <albertmokrejs@gmail.com>
2015-11-22 08:50:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request
 from itertools import chain
 from nltk.corpus import stopwords
 from operator import itemgetter
+from collections import OrderedDict
+from dateutil.parser import parse
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
@@ -15,6 +17,9 @@ def home():
     if re.findall("(who)",query.lower()):
         WhoFinal = whoSearch(query)
         return render_template("home.html", result = WhoFinal)
+    if re.findall("(when)".query.lower()):
+        WhenFinal = whenSearch(query)
+        return render_template("home.html", result = WhenFinal)
         
     return render_template("home.html")
 
@@ -93,6 +98,48 @@ def whoSearch(query):
             found = True
             Result = name[0]
     return Result
+    
+def whenSearch(query):
+    N = 10
+    results = google.search(query,num=N,start=0,stop=N)
+    rlist = []
+    for r in results:
+        rlist.append(r)
+    rawlist = []
+    rawString = ""
+    for x in rlist:
+        url = urllib2.urlopen(rlist[0])
+        raw = url.read()
+        text = re.sub("[ \t\n]+"," ",raw)
+        rawlist.append(text)
+        rawString = rawString + text + " "
+    
+    whenPattern = "((oct|nov|dec|jan|feb|mar|apr|may|jun|jul|aug|sep)\w* ((\d{1,2})\w{0,2})(,? (\d{2,4}))?)|((\d{1,4})[\/ \-](\d{1,4})[\/ \-](\d{1,4}))"
+    result = re.findall(whenPattern,rawString)
+    x = 0
+    while x < len(result):
+        result[x] = list(OrderedDict.fromkeys(result[x]))
+        x = x + 1
+    x = 0
+    print result
+    while x < len(result):
+        z = ""
+        for y in result[x]:
+            z = z + y
+        result[x] = parse(re.sub("[ \-\/]"," ",z)).strftime('%d/%m/%Y')
+        x = x+1
+    print result
+    
+    partSet = set(result)
+    partDict = {}
+    for part in partSet:
+        partDict[part] = 0
+    for part in result:
+        partDict[part] = partDict[part] + 1
+    
+    possibleDates = sorted(partDict.iteritems(), key=itemgetter(1), reverse=True)
+    
+    return possibleDates[0]
 
 if __name__ == "__main__":
    app.debug = True

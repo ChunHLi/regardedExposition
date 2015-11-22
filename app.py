5a7d458292e08409efb9a34f621da5079957b6f6
Albert Mokrejs <albertmokrejs@gmail.com>
2015-11-22 11:03:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -114,8 +114,9 @@ def whenSearch(query):
         rawlist.append(text)
         rawString = rawString + text + " "
     
-    whenPattern = "((?:oct|nov|dec|jan|feb|mar|apr|may|jun|jul|aug|sep)\w* (?:(?:\d{1,2})\w{0,2})(?:,? (?:\d{2,4}))?)|(?:(?:\d{1,4})[\/ \-](?:\d{1,4})[\/ \-](?:\d{1,4}))"
+    whenPattern = "((oct|nov|dec|jan|feb|mar|apr|may|jun|jul|aug|sep)\w* ((?:\d{1,2})\w{0,2})(?:,? (?:\d{2,4}))?)|(?:(?:\d{1,4})[\/ \-](\d{1,4})[\/ \-](?:\d{1,4}))"
     result = re.findall(whenPattern,rawString)
+    print result
     x = 0
     while x < len(result):
         result[x] = list(OrderedDict.fromkeys(result[x]))
